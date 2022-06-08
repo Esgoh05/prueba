11d579e0f960d8cc21608585c8ab752a52391c7a
Estefanía Gordillo <ada_hack143@hackademy.mx>
2022-06-08 15:51:21 -0500

    moreInfo

--- app/src/main/java/mx/uaemex/prueba/AcercaDeActivity.java
+++ app/src/main/java/mx/uaemex/prueba/AcercaDeActivity.java
@@ -18,6 +18,8 @@ public class AcercaDeActivity extends AppCompatActivity {
     private View Recomendaciones5;
     private View Recomendaciones6;
     private View Recomendaciones7;
+    private View Recomendaciones8;
+    private View Recomendaciones9;
     private RelativeLayout txtRecomendaciones;
     private RelativeLayout txtRecomendaciones1;
     private RelativeLayout txtRecomendaciones2;
@@ -26,6 +28,8 @@ public class AcercaDeActivity extends AppCompatActivity {
     private RelativeLayout txtRecomendaciones5;
     private RelativeLayout txtRecomendaciones6;
     private RelativeLayout txtRecomendaciones7;
+    private RelativeLayout txtRecomendaciones8;
+    private RelativeLayout txtRecomendaciones9;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,6 +54,8 @@ public class AcercaDeActivity extends AppCompatActivity {
             Recomendaciones5 = findViewById(R.id.txtRec2);
             Recomendaciones6 = findViewById(R.id.item_txtTip3);
             Recomendaciones7 = findViewById(R.id.txtRec3);
+            Recomendaciones8 = findViewById(R.id.item_txtTip4);
+            Recomendaciones9 = findViewById(R.id.item_MoreInfo);
             txtRecomendaciones = findViewById(R.id.relativeLayout3);
             txtRecomendaciones1 = findViewById(R.id.relativeLayout4);
             txtRecomendaciones3 = findViewById(R.id.relativeLayout1);
@@ -58,6 +64,8 @@ public class AcercaDeActivity extends AppCompatActivity {
             txtRecomendaciones5 = findViewById(R.id.relativeLayout2);
             txtRecomendaciones6 = findViewById(R.id.relativeLayout7);
             txtRecomendaciones7 = findViewById(R.id.relativeLayout8);
+            txtRecomendaciones8 = findViewById(R.id.relativeLayout9);
+            txtRecomendaciones9 = findViewById(R.id.relativeLayout10);
 
             Recomendaciones.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -70,6 +78,7 @@ public class AcercaDeActivity extends AppCompatActivity {
                         txtRecomendaciones2.setVisibility(View.INVISIBLE);
                         txtRecomendaciones4.setVisibility(View.INVISIBLE);
                         txtRecomendaciones7.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones9.setVisibility(View.INVISIBLE);
                     }
                     else {
                         txtRecomendaciones1.setVisibility(View.INVISIBLE);
@@ -89,6 +98,7 @@ public class AcercaDeActivity extends AppCompatActivity {
                         txtRecomendaciones1.setVisibility(View.INVISIBLE);
                         txtRecomendaciones4.setVisibility(View.INVISIBLE);
                         txtRecomendaciones7.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones9.setVisibility(View.INVISIBLE);
                     }
                     else {
                         txtRecomendaciones2.setVisibility(View.INVISIBLE);
@@ -105,6 +115,7 @@ public class AcercaDeActivity extends AppCompatActivity {
                         txtRecomendaciones1.setVisibility(View.INVISIBLE);
                         txtRecomendaciones2.setVisibility(View.INVISIBLE);
                         txtRecomendaciones7.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones9.setVisibility(View.INVISIBLE);
                     }
                     else {
                         txtRecomendaciones4.setVisibility(View.INVISIBLE);
@@ -124,9 +135,30 @@ public class AcercaDeActivity extends AppCompatActivity {
                         txtRecomendaciones2.setVisibility(View.INVISIBLE);
                         txtRecomendaciones4.setVisibility(View.INVISIBLE);
                         txtRecomendaciones1.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones9.setVisibility(View.INVISIBLE);
                     }
                     else {
                         txtRecomendaciones7.setVisibility(View.INVISIBLE);
+                    }
+
+                }
+            });
+
+            Recomendaciones8.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //txtRecomendaciones.setVisibility(View.GONE);
+                    //Toast.makeText(getApplicationContext(), "#Tip 2. Que tus gastos no excedan la cantidad de tus ingresos", Toast.LENGTH_LONG)
+                    //.show();
+                    if(Recomendaciones9.getVisibility() == View.VISIBLE){
+                        txtRecomendaciones9.setVisibility(View.VISIBLE);
+                        txtRecomendaciones1.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones4.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones7.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones2.setVisibility(View.INVISIBLE);
+                    }
+                    else {
+                        txtRecomendaciones9.setVisibility(View.INVISIBLE);
                     }
 
                 }
