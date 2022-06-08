f3cb553267e04f5819dde48ec69762963247cf2e
Estefanía Gordillo <ada_hack143@hackademy.mx>
2022-06-07 20:24:44 -0500

    recomendaciones

--- app/src/main/java/mx/uaemex/prueba/AcercaDeActivity.java
+++ app/src/main/java/mx/uaemex/prueba/AcercaDeActivity.java
@@ -2,20 +2,21 @@ package mx.uaemex.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class AcercaDeActivity extends AppCompatActivity {
+
+    private View Recomendaciones;
+    private View Recomendaciones1;
+    private View Recomendaciones2;
+    private RelativeLayout txtRecomendaciones;
+    private RelativeLayout txtRecomendaciones1;
+    private RelativeLayout txtRecomendaciones2;
+    private RelativeLayout txtRecomendaciones3;
 
     //@Override
     //protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +35,55 @@ public class AcercaDeActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setTheme(R.style.ChanceColor);
-            //setContentView(R.layout.activity_recomendaciones);
+            setContentView(R.layout.activity_recomendaciones);
             //setContentView(R.layout.activity_rec);
-            setContentView(R.layout.activity_acercade);
+            //setContentView(R.layout.activity_acercade);
             setTitle("Recomendaciones ");
 
+            Recomendaciones = findViewById(R.id.item_txtTip2);
+            Recomendaciones1 = findViewById(R.id.item_txtRec1);
+            Recomendaciones2 = findViewById(R.id.txtRec);
+            txtRecomendaciones = findViewById(R.id.relativeLayout3);
+            txtRecomendaciones1 = findViewById(R.id.relativeLayout4);
+            txtRecomendaciones3 = findViewById(R.id.relativeLayout1);
+            txtRecomendaciones2 = findViewById(R.id.relativeLayout5);
+
+            Recomendaciones.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //txtRecomendaciones.setVisibility(View.GONE);
+                    //Toast.makeText(getApplicationContext(), "#Tip 2. Que tus gastos no excedan la cantidad de tus ingresos", Toast.LENGTH_LONG)
+                    //.show();
+                    if(Recomendaciones1.getVisibility() == View.VISIBLE){
+                        txtRecomendaciones1.setVisibility(View.VISIBLE);
+                        txtRecomendaciones.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones2.setVisibility(View.INVISIBLE);
+                    }
+                    else {
+                        txtRecomendaciones1.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            });
+
+            Recomendaciones2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //txtRecomendaciones.setVisibility(View.GONE);
+                    //Toast.makeText(getApplicationContext(), "#Tip 2. Que tus gastos no excedan la cantidad de tus ingresos", Toast.LENGTH_LONG)
+                    //.show();
+                    if(Recomendaciones1.getVisibility() == View.VISIBLE){
+                        txtRecomendaciones1.setVisibility(View.VISIBLE);
+                        txtRecomendaciones.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones2.setVisibility(View.INVISIBLE);
+                    }
+                    else {
+                        txtRecomendaciones1.setVisibility(View.INVISIBLE);
+                        txtRecomendaciones.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            });
         }
     }
